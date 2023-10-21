76ea75417bf015cd6d2aafd41c3a8e968660fa3b
Thayna Rodrigues <thna.rdg@gmail.com>
2023-10-21 16:35:59 -0300

    adding all videos

--- spript.js
+++ spript.js
@@ -1,23 +1,47 @@
-var video = document.querySelector("video");
+// NAVIGATION BAR
+let nav = document.getElementById("nav");
+let ul = document.createElement("ul");
+let listItems = ["Home", "About", "Contact"];
 
-video.addEventListener("mouseenter", function () {
-  video.play();
-});
+for (let i = 0; i < listItems.length; i++) {
+  let li = document.createElement("li");
+  let a = document.createElement("a");
+  a.setAttribute("href", "/");
+  a.textContent = listItems[i];
+  li.appendChild(a);
+  ul.appendChild(li);
+}
+nav.appendChild(ul);
 
-video.addEventListener("mouseleave", function () {
-  video.currentTime = 0;
-  video.pause();
-});
-
-var videos = [
+// VIDEO GALLERY
+var allAIVideos = [
   "assets/videos/AI/UncannyLandscapesTrim.mp4",
   "assets/videos/AI/ThrivingPondTrim.mp4",
   "assets/videos/AI/FungalTexturesTrim.mp4",
   "assets/videos/AI/SilentPortrayalsTrim.mp4",
-  "assets/videos/3D/CONTACT.mp4",
-  "assets/videos/3D/NEWDAY.mp4",
-  "assets/videos/3D/WANDERER.mp4",
-  "assets/videos/AUTORAIS/SobaLenteTrim.mp4",
+];
+
+var all3DVideos = [
+  "assets/videos/3D/3DCONTACT.mp4",
+  "assets/videos/3D/3DNEWDAY.mp4",
+  "assets/videos/3D/3DWANDERER.mp4",
+];
+
+var allAutoraisVideos = ["assets/videos/AUTORAIS/SobaLenteTrim.mp4"];
+
+var allComercialVideos = [
+  "assets/videos/COMERCIAL/AmazoniaemEXAMEIGTVTrim.mp4",
+  "assets/videos/COMERCIAL/EnodiaAtelierTrim.mp4",
+  "assets/videos/COMERCIAL/JulioOkuboCampanhaMãesTrim.mp4",
+  "assets/videos/COMERCIAL/NaturaxÁrvoreShowReelTrim.mp4",
+];
+
+// All video arrays
+var allVideos = [
+  allAIVideos,
+  all3DVideos,
+  allAutoraisVideos,
+  allComercialVideos,
 ];
 
 // Function to create a new video element
@@ -36,8 +60,12 @@ function createVideo(src) {
 // Get the container element
 var container = document.getElementById("videoGallery");
 
-// Add each video to the container
-for (var i = 0; i < videos.length; i++) {
-  var videoElement = createVideo(videos[i]);
-  container.appendChild(videoElement);
+// Iterate over each video array
+for (var i = 0; i < allVideos.length; i++) {
+  // Iterate over each video in the current array
+  for (var j = 0; j < allVideos[i].length; j++) {
+    // Create a new video element and append it to the container
+    var videoElement = createVideo(allVideos[i][j]);
+    container.appendChild(videoElement);
+  }
 }

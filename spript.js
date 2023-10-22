9be6cc6fc2cc5b42f418e76f9f369d7684bb970d
Thayna Rodrigues <thna.rdg@gmail.com>
2023-10-22 17:15:51 -0300

    adding new portrait video

--- spript.js
+++ spript.js
@@ -36,59 +36,27 @@ sr.reveal(`#home, #nav-bar, #about, #works, #contact`, {
   interval: 100,
 });
 
-// PHOTO GALLERY
-var allPhotos = {
-  "assets/images/mobile/8.png":
-    "https://player.vimeo.com/video/859910013?h=61d063f865",
-  "assets/images/mobile/6.png": "https://www.youtube.com/embed/eCDQTyKjKTA",
-  "assets/images/mobile/4.png": "https://www.youtube.com/embed/u6oMLGfMSoY",
-  "assets/images/mobile/7.png":
-    "https://www.youtube.com/embed/YQnj-8tnS_E?si=oX0UGwtu1ni3qneG",
-  "assets/images/mobile/1.png":
-    "https://player.vimeo.com/video/770011545?h=7249150444",
-  "assets/images/mobile/2.png":
-    "https://player.vimeo.com/video/770011738?h=ef80a1b7d0",
-  "assets/images/mobile/3.png":
-    "https://player.vimeo.com/video/770011127?h=3278f5c2f8",
-  "assets/images/mobile/9.png":
-    "https://player.vimeo.com/video/731179431?h=9b87222ada",
-  "assets/images/mobile/10.png":
-    "https://player.vimeo.com/video/656586224?h=3af300f78c",
-  "assets/images/mobile/11.png":
-    "https://player.vimeo.com/video/727426164?h=7f2c341a7c",
-  "assets/images/mobile/12.png":
-    "https://player.vimeo.com/video/790509577?h=5ff4cd06ea",
-  "assets/images/mobile/13.png":
-    "https://www.youtube.com/embed/Iv33denEOys?si=cBROiExkcF5Bzd7x",
-};
-
 // VIDEO GALLERY
-var allAIVideos = {
+// Endereço dos vídeos e link pro embed
+var allVideos = {
   "assets/videos/AI/UncannyLandscapesTrim.mp4":
     "https://player.vimeo.com/video/859910013?h=61d063f865",
+  "assets/videos/AI/GreenLab.mp4":
+    "https://player.vimeo.com/video/876914728?h=465b55533c",
   "assets/videos/AI/ThrivingPondTrim.mp4":
-    "https://www.youtube.com/embed/eCDQTyKjKTA",
+    "https://player.vimeo.com/video/876914555?h=cbef4bb827",
   "assets/videos/AI/FungalTexturesTrim.mp4":
     "https://www.youtube.com/embed/u6oMLGfMSoY",
   "assets/videos/AI/SilentPortrayalsTrim.mp4":
     "https://www.youtube.com/embed/YQnj-8tnS_E?si=oX0UGwtu1ni3qneG",
-};
-
-var all3DVideos = {
   "assets/videos/3D/3DCONTACT.mp4":
     "https://player.vimeo.com/video/770011545?h=7249150444",
   "assets/videos/3D/3DNEWDAY.mp4":
     "https://player.vimeo.com/video/770011738?h=ef80a1b7d0",
   "assets/videos/3D/3DWANDERER.mp4":
     "https://player.vimeo.com/video/770011127?h=3278f5c2f8",
-};
-
-var allAutoraisVideos = {
   "assets/videos/AUTORAIS/SobaLenteTrim.mp4":
     "https://player.vimeo.com/video/731179431?h=9b87222ada",
-};
-
-var allComercialVideos = {
   "assets/videos/COMERCIAL/AmazoniaemEXAMEIGTVTrim.mp4":
     "https://player.vimeo.com/video/656586224?h=3af300f78c",
   "assets/videos/COMERCIAL/EnodiaAtelierTrim.mp4":
@@ -99,18 +67,12 @@ var allComercialVideos = {
     "https://www.youtube.com/embed/Iv33denEOys?si=cBROiExkcF5Bzd7x",
 };
 
-var allVideos = [
-  allAIVideos,
-  all3DVideos,
-  allAutoraisVideos,
-  allComercialVideos,
-];
-
 function createVideo(src, vimeoUrl) {
   var video = document.createElement("video");
   video.src = src;
   video.controls = false;
   video.muted = true;
+  video.loop = true;
   video.style.opacity = 1;
   video.addEventListener("loadedmetadata", function () {
     if (this.videoWidth > this.videoHeight) {
@@ -155,9 +117,7 @@ function createVideo(src, vimeoUrl) {
     }
 
     modal.appendChild(embedContainer);
-
     document.body.appendChild(modal);
-
     embedContainer.onclick = function (event) {
       if (event.target == embedContainer) {
         document.body.removeChild(modal);
@@ -167,17 +127,44 @@ function createVideo(src, vimeoUrl) {
 
   var div = document.createElement("div");
   div.appendChild(video);
-
   return div;
 }
 
 var container = document.getElementById("videoGallery");
-for (var category in allVideos) {
-  for (var src in allVideos[category]) {
-    var videoElement = createVideo(src, allVideos[category][src]);
-    container.appendChild(videoElement);
-  }
+for (var src in allVideos) {
+  var videoElement = createVideo(src, allVideos[src]);
+  container.appendChild(videoElement);
 }
+
+// PHOTO GALLERY
+// Endereço das fotos e link pro embed
+var allPhotos = {
+  "assets/images/mobile/8.png":
+    "https://player.vimeo.com/video/859910013?h=61d063f865",
+  "assets/images/mobile/5.png":
+    "https://player.vimeo.com/video/876914728?h=465b55533c",
+  "assets/images/mobile/6.png":
+    "https://player.vimeo.com/video/876914555?h=cbef4bb827",
+  "assets/images/mobile/4.png": "https://www.youtube.com/embed/u6oMLGfMSoY",
+  "assets/images/mobile/7.png":
+    "https://www.youtube.com/embed/YQnj-8tnS_E?si=oX0UGwtu1ni3qneG",
+  "assets/images/mobile/1.png":
+    "https://player.vimeo.com/video/770011545?h=7249150444",
+  "assets/images/mobile/2.png":
+    "https://player.vimeo.com/video/770011738?h=ef80a1b7d0",
+  "assets/images/mobile/3.png":
+    "https://player.vimeo.com/video/770011127?h=3278f5c2f8",
+  "assets/images/mobile/9.png":
+    "https://player.vimeo.com/video/731179431?h=9b87222ada",
+  "assets/images/mobile/10.png":
+    "https://player.vimeo.com/video/656586224?h=3af300f78c",
+  "assets/images/mobile/11.png":
+    "https://player.vimeo.com/video/727426164?h=7f2c341a7c",
+  "assets/images/mobile/12.png":
+    "https://player.vimeo.com/video/790509577?h=5ff4cd06ea",
+  "assets/images/mobile/13.png":
+    "https://www.youtube.com/embed/Iv33denEOys?si=cBROiExkcF5Bzd7x",
+};
 
 function createImage(src, vimeoUrl) {
   var img = document.createElement("img");
